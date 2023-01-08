66564cd97db8f08c6386a1733374f51430b8836c
hong <rlaxoxo5252@gmail.com>
2023-01-09 00:54:23 +0900

    로그추가

--- app/src/main/java/com/opensurvey/uitext/MainActivity.kt
+++ app/src/main/java/com/opensurvey/uitext/MainActivity.kt
@@ -2,12 +2,27 @@ package com.opensurvey.uitext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainbtn.setOnClickListener {
+
+            Log.d("메인버튼","메인화면클릭확인")
+
+        }
+
+        subbtn.setOnClickListener {
+
+
+
+        }
 
 
 
